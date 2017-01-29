b29b75a47d9ec94751a6df778a5e25bfbbfb97dd
zaczhang19 <zaczhang19@gmail.com>
2017-01-28 22:53:19 -0500

    framework

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,5 +1,37 @@
-/**
- * Created by zhangzhichao on 2017-01-15.
- */
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class WordCount {
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
 }
