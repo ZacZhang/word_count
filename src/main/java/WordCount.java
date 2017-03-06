0114a0c3e1888b2605be36774475a70fcd44161d
zaczhang19 <zaczhang19@gmail.com>
2017-03-05 22:40:47 -0500

    main function finished

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,6 +1,3 @@
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -10,7 +7,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.IOException;
 
+// delete output file before execution
 public class WordCount {
 
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
@@ -18,16 +17,18 @@ public class WordCount {
 
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            //key: offset
-            //value: line
-            //example value: I love data
+            // key: offset
+            // value: line 读进来的一句话
+            // example value: I love data
+
+            // split into words
             String[] words = value.toString().split(" ");
             for (String word: words) {
-                //I 1
-                //love 1
-                //data 1
-                Text outputKey = new Text(word);//key is the word
-                IntWritable outputValue = new IntWritable(1);//先假设每个单词出现一次，最后再相加，统计工作由reducer完成
+                // I 1
+                // love 1
+                // data 1
+                Text outputKey = new Text(word);// key is the word
+                IntWritable outputValue = new IntWritable(1);// 先假设每个单词出现一次，最后再相加，统计工作由reducer完成
                 context.write(outputKey, outputValue);
             }
 
@@ -38,8 +39,8 @@ public class WordCount {
 
         @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            //I <1>
-            //love <1,1> if love appears twice
+            // I <1>
+            // love <1,1> if love appears twice
             int sum = 0;
             for (IntWritable value: values) {
                 sum = sum + value.get();
@@ -50,14 +51,17 @@ public class WordCount {
 
     public static void main(String[] args) throws Exception {
         Configuration configuration = new Configuration();
+
         Job job = Job.getInstance(configuration);
         job.setJarByClass(WordCount.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setReducerClass(WordNumberReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        FileInputFormat.addInputPath(job, new Path(args[0])); // 通过命令行来设置输入输出路径
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.waitForCompletion(true);
+
+        job.waitForCompletion(true); // 等待上一个mapreduce结束才能进行下一个mapreduce
     }
 }
